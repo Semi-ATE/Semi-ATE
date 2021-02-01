db67f2bdec0b46a62a8ddb87014ed085aa092ff0
seimit <kkos@semi-ate.com>
2021-02-01 23:18:59 +0200

    Temporary fix to pass the test

--- ATE/data/STDF/__init__.py
+++ ATE/data/STDF/__init__.py
@@ -2,4 +2,5 @@ import os
 
 from ATE.data.STDF.STDR import STDR
 from ATE.data.STDF.STDR import get_STDF_setup_from_file
-from ATE.data.STDF.STDR import records_from_file
+from ATE.data.STDF.STDR import ts_to_id
+from ATE.data.STDF.STDR import supported
