d383087404fcfc4d17f58a8a3fb5dd3eccb2b43c
Tom Hören <horen.tom@gmail.com>
2021-02-10 08:39:05 +0100

    added workflow for 'test-windows', touched up workflow 'test-linux'

--- ATE/data/STDF/__init__.py
+++ ATE/data/STDF/__init__.py
@@ -1,29 +1,29 @@
 import os
 
 from ATE.data.STDF import STDR
-from ATE.data.STDF import ATR
-from ATE.data.STDF import BPS
-from ATE.data.STDF import DTR
-from ATE.data.STDF import EPS
-from ATE.data.STDF import FAR
-from ATE.data.STDF import FTR
-from ATE.data.STDF import GDR
-from ATE.data.STDF import HBR
-from ATE.data.STDF import MIR
-from ATE.data.STDF import MPR
-from ATE.data.STDF import MRR
-from ATE.data.STDF import PCR
-from ATE.data.STDF import PGR
-from ATE.data.STDF import PIR
-from ATE.data.STDF import PLR
-from ATE.data.STDF import PMR
-from ATE.data.STDF import PRR
-from ATE.data.STDF import PTR
-from ATE.data.STDF import RDR
-from ATE.data.STDF import SBR
-from ATE.data.STDF import SDR
-from ATE.data.STDF import TSR
-from ATE.data.STDF import WCR
-from ATE.data.STDF import WIR
-from ATE.data.STDF import WRR
-from ATE.data.STDF import utils
+from ATE.data.STDF.ATR import ATR
+# from ATE.data.STDF import BPS
+# from ATE.data.STDF import DTR
+# from ATE.data.STDF import EPS
+# from ATE.data.STDF import FAR
+# from ATE.data.STDF import FTR
+# from ATE.data.STDF import GDR
+# from ATE.data.STDF import HBR
+# from ATE.data.STDF import MIR
+# from ATE.data.STDF import MPR
+# from ATE.data.STDF import MRR
+# from ATE.data.STDF import PCR
+# from ATE.data.STDF import PGR
+# from ATE.data.STDF import PIR
+# from ATE.data.STDF import PLR
+# from ATE.data.STDF import PMR
+# from ATE.data.STDF import PRR
+# from ATE.data.STDF import PTR
+# from ATE.data.STDF import RDR
+# from ATE.data.STDF import SBR
+# from ATE.data.STDF import SDR
+# from ATE.data.STDF import TSR
+# from ATE.data.STDF import WCR
+# from ATE.data.STDF import WIR
+# from ATE.data.STDF import WRR
+# from ATE.data.STDF import utils
