49954954d7c1f0ee63e73bd3c497a66c321f5033
dsk <github@awinia.de>
2022-03-16 16:49:39 +0100

    [#133] Add version and licence text to packages

---
+++ build-tools/package_list.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+git_root_folder = Path(Path(__file__).parent, '../')
+
+distribution_packages = [
+    {
+        'name': 'semi-ate-common',
+        'dir': Path(git_root_folder, 'src/ATE_common'),
+        'namespace': 'ate_common'
+    },
+    {
+        'name': 'semi-ate-project-database',
+        'dir': Path(git_root_folder, 'src/ATE_projectdatabase'),
+        'namespace': 'ate_projectdatabase'
+    },
+    {
+        'name': 'semi-ate-sammy',
+        'dir': Path(git_root_folder, 'src/ATE_sammy'),
+        'namespace': 'ate_sammy'},
+    {
+        'name': 'semi-ate-plugins',
+        'dir': Path(git_root_folder, 'src/ATE_semiateplugins'),
+        'namespace': 'ate_semiateplugins'
+    },
+    {
+        'name': 'semi-ate-spyder',
+        'dir': Path(git_root_folder, 'src/ATE_spyder'),
+        'namespace': 'ate_spyder'
+    },
+    {
+        'name': 'semi-ate-apps-common',
+        'dir': Path(git_root_folder, 'src/Apps/common'),
+        'namespace': 'ate_apps_common'
+    },
+    {
+        'name': 'semi-ate-control-app',
+        'dir': Path(git_root_folder, 'src/Apps/control_app'),
+        'namespace': 'ate_control_app'
+    },
+    {
+        'name': 'semi-ate-master-app',
+        'dir': Path(git_root_folder, 'src/Apps/master_app'),
+        'namespace': 'ate_master_app'
+    },
+    {
+        'name': 'semi-ate-test-app',
+        'dir': Path(git_root_folder, 'src/Apps/test_app'),
+        'namespace': 'ate_test_app'
+    },
+    {
+        'name': 'semi-ate-handler-app',
+        'dir': Path(git_root_folder, 'src/Apps/handler_app'),
+        'namespace': 'ate_handler_app'
+    },
+    {
+        'name': 'tdk-micronas',
+        'dir': Path(git_root_folder, 'src/Plugins/TDKMicronas'),
+        'namespace': 'TDKMicronas'
+    },
+]
+
+integration_test_packages = [
+    {
+        'name': 'integration-test-common',
+        'dir': Path(git_root_folder, 'src/integration_tests/Plugins/Common'),
+        'namespace': ''
+    },
+    {
+        'name': 'dummy-plugin',
+        'dir': Path(git_root_folder, 'src/integration_tests/Plugins/DummyTester'),
+        'namespace': 'DummyTester'
+    },
+]
--- build-tools/package_tool.py
+++ build-tools/package_tool.py
@@ -1,8 +1,11 @@
 import argparse
+import fileinput
 from pathlib import Path
 from subprocess import Popen
 from enum import Enum
-from typing import List
+import sys
+from typing import List, Union
+from package_list import distribution_packages, integration_test_packages
 
 class SetupCommand(Enum):
     Install = 'install'
@@ -12,6 +15,7 @@ class SetupCommand(Enum):
     def __call__(self):
         return self.value
 
+
 class Package(Enum):
     All = 'all',
     Distribution = 'distribution'
@@ -20,12 +24,15 @@ class Package(Enum):
     def __call__(self):
         return self.value
 
+
 class PackageType(Enum):
     Name = 0
-    Path = 1
+    SetupDirPath = 1
+    InitDirPath = 2
 
     def __call__(self):
         return self.value
+
 
 class Profile(Enum):
     Develop = 'develop'
@@ -36,27 +43,8 @@ class Profile(Enum):
         return self.value
 
 git_root_folder = Path(Path(__file__).parents[0], '../')
-
-distribution_packages = [
-    ['semi-ate-common', Path(git_root_folder, 'src/ATE_common')],
-    ['semi-ate-project-database', Path(git_root_folder, 'src/ATE_projectdatabase')],
-    ['semi-ate-sammy', Path(git_root_folder, 'src/ATE_sammy')],
-    ['semi-ate-plugins', Path(git_root_folder, 'src/ATE_semiateplugins')],
-    ['semi-ate-spyder', Path(git_root_folder, 'src/ATE_spyder')],
-    ['semi-ate-apps-common', Path(git_root_folder, 'src/Apps/common')],
-    ['semi-ate-control-app', Path(git_root_folder, 'src/Apps/control_app')],
-    ['semi-ate-master-app', Path(git_root_folder, 'src/Apps/master_app')],
-    ['semi-ate-test-app', Path(git_root_folder, 'src/Apps/test_app')],
-    ['semi-ate-handler-app', Path(git_root_folder, 'src/Apps/handler_app')],
-    ['tdk-micronas', Path(git_root_folder, 'src/Plugins/TDKMicronas')],
-]
-
-integration_test_packages = [
-    ['integration-test-common', Path(git_root_folder, 'src/integration_tests/Plugins/Common')],
-    ['dummy-plugin', Path(git_root_folder, 'src/integration_tests/Plugins/DummyTester')],
-]
-
 integration_tests_path = Path(git_root_folder, 'src/integration_tests')
+
 
 def uninstall(packages: Package):
     package_list = _compute_package_list(packages, PackageType.Name)
@@ -65,8 +53,9 @@ def uninstall(packages: Package):
         process = Popen(['python', '-m', 'pip', 'uninstall', '-y', '-q', p])
         process.wait()
 
+
 def setup(packages: Package, setup_command: SetupCommand):
-    package_list = _compute_package_list(packages,  PackageType.Path)
+    package_list = _compute_package_list(packages, PackageType.SetupDirPath)
     for p in package_list:
         path = Path(Path(__file__).parents[0], p)
         if path.exists() == True:
@@ -84,6 +73,7 @@ def setup(packages: Package, setup_command: SetupCommand):
                 process.wait()
         else:
             print(f'ERROR: Path "{path}" could not be found!')
+
 
 def change_environment(profile: Profile):
     if profile == Profile.Clean:
@@ -105,7 +95,20 @@ def change_environment(profile: Profile):
     process.wait()
 
 
-def _compute_package_list(package_type: Package, type: PackageType) -> List[str]:
+def tag_version(packages: Package, version: str):
+    package_list = _compute_package_list(packages, PackageType.InitDirPath)
+    for p in package_list:
+        path = Path(p, '__init__.py')
+        licence_path = Path(Path(__file__).parent, '../LICENSE.txt')
+        with path.open('a+') as init_file:
+            init_file.write('__license__ = """\n')
+            with licence_path.open('r') as license_file:
+                for line in license_file:
+                    init_file.write(line)
+            init_file.write('"""\n')
+            init_file.write(f'__version__ = \'{version}\'\n')
+
+def _compute_package_list(package_type: Package, type: PackageType) -> Union[List[str], List[Path]]:
     if package_type == Package.Distribution:
         packages = distribution_packages
     elif package_type == Package.All:
@@ -113,7 +116,15 @@ def _compute_package_list(package_type: Package, type: PackageType) -> List[str]
         packages.extend(integration_test_packages)
     else:
         packages = integration_test_packages
-    return list(map(lambda entry: entry[type()],packages))
+    
+    if type == PackageType.Name:
+        return list(map(lambda entry: entry['name'], packages))
+    elif type == PackageType.SetupDirPath:
+        return list(map(lambda entry: entry['dir'], packages))
+    else:
+        # directory path to __init__.py package file
+        return list(map(lambda entry: Path(entry['dir'], entry['namespace']), packages))
+
 
 def _command_from_string(value: str) -> SetupCommand:
     if value == SetupCommand.Develop():
@@ -123,13 +134,15 @@ def _command_from_string(value: str) -> SetupCommand:
     else:
         return SetupCommand.Sdist
 
+
 def _package_from_string(value: str) -> Package:
     if value == Package.All():
         return Package.All
     elif value == Package.Test():
         return Package.Test
     else:
-        return Package.Distribution        
+        return Package.Distribution
+
 
 def _profile_from_string(value: str) -> Profile:
     if value == Profile.Develop():
@@ -137,17 +150,19 @@ def _profile_from_string(value: str) -> Profile:
     elif value == Profile.Cicd():
         return Profile.Cicd
     else:
-        return Profile.Clean     
+        return Profile.Clean
+
 
 def _collect_test_requirements(include_cicd: bool) -> List[Path]:
-    distribution_packages_paths = _compute_package_list(Package.Distribution, PackageType.Path)
+    distribution_packages_paths = _compute_package_list(Package.Distribution, PackageType.SetupDirPath)
     distribution_packages_paths.append(integration_tests_path)
     path_list = list(map(lambda entry: Path(entry, 'requirements/test.txt'), distribution_packages_paths))
     if include_cicd == True:
         path_list.append(Path(git_root_folder, 'requirements/cicd.txt'))
     return path_list
 
-def _collect_packages_from_paths(paths: List[Path])-> List[str]:
+
+def _collect_packages_from_paths(paths: List[Path]) -> List[str]:
     packages = set()
     for p in paths:
         if p.exists() == True:
@@ -156,12 +171,14 @@ def _collect_packages_from_paths(paths: List[Path])-> List[str]:
                     packages.add(line.strip())
     return list(packages)
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--setup-cmd', choices=[SetupCommand.Develop(), SetupCommand.Install(), SetupCommand.Sdist()], help='runs "python setup.py" with the provided value')
     parser.add_argument('--packages', choices=[Package.Distribution(), Package.Test(), Package.All()], help='packages that should be processed')
     parser.add_argument('--uninstall', action='store_true', help='uninstall the selected packages from environment using "pip"')
     parser.add_argument('--change-env', choices=[Profile.Develop(), Profile.Cicd(), Profile.Clean()], help='Setup/Installs all packages to the current environment to be ready for developing or running CI/CD scripts. To do so this option relies on "pip".')
+    parser.add_argument('--tag-version', type=str, help='Version to write into "__init__.py" package files')
 
     args = parser.parse_args()
     setup_command = SetupCommand.Develop if args.setup_cmd == None else _command_from_string(args.setup_cmd)
@@ -169,10 +186,12 @@ def main():
 
     if args.uninstall == True:
         uninstall(packages, PackageType.Name)
-    elif args.change_env == None:
-        setup(packages, setup_command)
-    else:
+    elif args.change_env != None:
         change_environment(_profile_from_string(args.change_env))
+    elif args.tag_version != None:
+        tag_version(packages, args.tag_version)
+    else:
+        setup(packages, setup_command)
 
 
 if __name__ == '__main__':
