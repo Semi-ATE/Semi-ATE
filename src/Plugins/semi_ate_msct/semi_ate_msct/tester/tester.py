9fd563521e1e49cf5627c25b8a3bb8984580a428
modelltechnik-jung <modelltechnik-jung@web.de>
2022-05-18 19:08:16 +0200

    issue 158

---
+++ src/Plugins/semi_ate_msct/semi_ate_msct/tester/tester.py
@@ -0,0 +1,25 @@
+try:
+    from SCT8.tester import Tester as sct8
+except ImportError:
+    from semi_ate_msct.tester.dummytester import Dummysct as sct8
+
+
+class Tester(sct8):
+
+    def get_sites_count(self):
+        return 1
+
+    def do_request(self, site_id: int, timeout: int) -> bool:
+        return True
+
+    def test_in_progress(self, site_id: int):
+        print(f'Tester.test_in_progress({site_id})')
+        pass
+
+    def test_done(self, site_id: int, timeout: int):
+        print(f'Tester.test_done({site_id})')
+        pass
+
+    def do_init_state(self, site_id: int):
+        print(f'Tester.do_init_state({site_id})')
+        pass
