e4a78d0b81aa36a20957a6508b711e33277a4dfb
awinia gmbh <github@awinia.de>
2022-06-23 12:16:07 +0200

    Add documentation (#178)
    * [#157] Add package documentation
    
    * [#157] Extend diagrams for documentation
    
    - Diagrams used for documentation has been extended
    - Two small typos have been corrected in py-files
    
    * [#157] Add tester diagrams
    
    * [#157] Add documentation for semi-ate applications

--- src/Apps/test_app/ate_test_app/actuators/Pressure/Pressure.py
+++ src/Apps/test_app/ate_test_app/actuators/Pressure/Pressure.py
@@ -1,8 +1,9 @@
 from ate_test_app.sequencers.MqttClient import MqttClient
 
 
-class PressureProxy():
+class PressureProxy:
     def set_mqtt_client(self, mqtt: MqttClient):
         self.mqtt = mqtt
+
 
 # TBD: API for the Pressure Actuator
