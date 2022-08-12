f5a50b8e4590c3e124243fcc6e4e6a7b4c3e9c3c
awinia gmbh <github@awinia.de>
2022-08-12 11:16:38 +0200

    Bug/215 fix migration 8 (#218)
    * #215 add missing migration
    
    * [no-issue] fix test selection

--- src/ATE_sammy/ate_sammy/migration/migration_scripts/migration_version_8.py
+++ src/ATE_sammy/ate_sammy/migration/migration_scripts/migration_version_8.py
@@ -10,11 +10,14 @@ class MigrationVersion8(MigratorBase):
             Path(defs_path).joinpath('hardware'), Path(defs_path).joinpath('masksets'), Path(defs_path).joinpath('die'),
             Path(defs_path).joinpath('package'), Path(defs_path).joinpath('device'), Path(defs_path).joinpath('product'),
             Path(defs_path).joinpath('group'), Path(defs_path).joinpath('program'), Path(defs_path).joinpath('qualification'),
-            Path(defs_path).joinpath('test'), Path(defs_path).joinpath('testtarget')
+            Path(defs_path).joinpath('test')
         ]
 
         for category_config_path in all_categories_config_path:
             self.migrate_section(category_config_path, lambda path: self._migrate_program(path))
+
+        self.migrate_section(Path(defs_path).joinpath('testtarget'), lambda path: self._migrate_testtargets(path))
+
         return 0
 
     def version_num(self) -> int:
@@ -38,6 +41,31 @@ class MigrationVersion8(MigratorBase):
             path = file_path.parent.joinpath(f'{basename}{name}.json')
             data = [category_config]
             self.write_configuration(path, data)
+
+        import os
+        os.remove(file_name)
+
+    def _migrate_testtargets(self, file_name):
+        category_config_list = self.read_configuration(file_name)
+        target_info = {}
+        for category_config in category_config_list:
+            name = None
+            if category_config.get('name'):
+                name = category_config['name']
+
+            if category_config.get('prog_name'):
+                name = category_config['prog_name']
+
+            if not name:
+                raise Exception('category name could not be found')
+
+            file_path = Path(file_name)
+            basename = file_path.parent.name
+            path = file_path.parent.joinpath(f'{basename}{name}.json')
+            target_info.setdefault(path, []).append(category_config)
+
+        for key, values in target_info.items():
+            self.write_configuration(key, values)
 
         import os
         os.remove(file_name)
