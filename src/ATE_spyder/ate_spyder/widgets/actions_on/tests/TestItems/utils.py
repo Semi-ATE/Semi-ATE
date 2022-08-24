549c3964b2db0113b43416c81925c4f4785e03c7
ar <github@awinia.de>
2022-08-24 15:18:09 +0200

    # clean up

--- src/ATE_spyder/ate_spyder/widgets/actions_on/tests/TestItems/utils.py
+++ src/ATE_spyder/ate_spyder/widgets/actions_on/tests/TestItems/utils.py
@@ -1,9 +1,66 @@
-from ate_spyder.widgets.navigation import ProjectNavigation
-from ate_spyder.widgets.actions_on.utils.MenuDialog import ImportDialog, Action, StandardDialog
+from enum import IntEnum
 import json
 import re
 from pathlib import Path
 from pydantic import BaseModel
+
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+
+from ate_spyder.widgets.actions_on.utils.BaseDialog import BaseDialog
+from ate_spyder.widgets.navigation import ProjectNavigation
+from ate_spyder.widgets.actions_on.utils.MenuDialog import StandardDialog
+
+
+class Action(IntEnum):
+    Rename = 0
+    Overwrite = 1
+
+
+class ImportDialog(BaseDialog):
+    def __init__(self, parent: object, project_info: ProjectNavigation, hardware: str, base: str):
+        ui_file = Path(__file__).parent.joinpath('ImportDialog.ui')
+        super().__init__(str(ui_file), parent)
+        self.project_info = project_info
+        self.hardware = hardware
+        self.base = base
+        self._connect_action_handler()
+
+    def _connect_action_handler(self):
+        from ate_spyder.widgets.validation import valid_name_regex
+        test_name_validator = QtGui.QRegExpValidator(QtCore.QRegExp(valid_name_regex), self)
+        self.fileName.setValidator(test_name_validator)
+        self.fileName.textChanged.connect(self._rename)
+
+        self.rename.clicked.connect(self.accept)
+        self.rename.setEnabled(False)
+
+        self.overwrite.clicked.connect(self._overwrite)
+        self.cancel.clicked.connect(self._cancel)
+        self.feedback.setText('')
+        self.feedback.setStyleSheet('color: orange')
+
+    def _cancel(self):
+        self.reject()
+
+    def _rename(self):
+        name = self.fileName.text()
+        if not name:
+            self.feedback.setText('make sure to insert a name')
+            self.rename.setEnabled(False)
+            return
+
+        if self.project_info.does_test_exist(name, self.hardware, self.base):
+            self.feedback.setText('test name already exists')
+            self.rename.setEnabled(False)
+            return
+
+        self.action = {'type': Action.Rename, 'data': {'name': name}}
+        self.rename.setEnabled(True)
+
+    def _overwrite(self):
+        self.action = {'type': Action.Overwrite, 'data': {}}
+        self.accept()
 
 
 class FileStruct(BaseModel):
@@ -39,7 +96,7 @@ def import_content(path: Path, project_info: ProjectNavigation, parent: object, 
         update_test_file_impl(''.join(file_struct.file), project_info, file_struct.database)
         return
 
-    wizard = ImportDialog(parent)
+    wizard = ImportDialog(parent, project_info, file_struct.database['hardware'], file_struct.database['base'])
     if not wizard.exec_():
         # cancel will return with a none value
         return
