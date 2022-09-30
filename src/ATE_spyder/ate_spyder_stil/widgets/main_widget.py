97b4181e4594e298c26193573b662a8f98784f25
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-09-29 20:10:14 -0500

    Display compiler info output on the log tab
    Co-authored-by: Stephannie Jimenez Gacha <steff456@hotmail.com>

--- src/ATE_spyder/ate_spyder_stil/widgets/main_widget.py
+++ src/ATE_spyder/ate_spyder_stil/widgets/main_widget.py
@@ -9,6 +9,7 @@
 # Standard library imports
 import os
 import os.path as osp
+from datetime import datetime
 import logging
 from typing import List, Optional
 
@@ -128,6 +129,17 @@ class STILContainer(PluginMainWidget):
         if not self.project_info:
             return
 
+        self.output_log.clear()
+        self.output_tree.clear()
+        self.tabwidget.setCurrentIndex(1)
+
+        now = datetime.now()
+        self.publish_to_log(
+            '----------------- '
+            f'Compilation started {now.isoformat()} '
+            '-----------------', skip_time=True
+        )
+
         self.stil_process = QProcess(self)
         # self.stil_process.errorOccurred.connect(self.stil_process_failed)
         self.stil_process.finished.connect(self.stil_process_finished)
@@ -175,6 +187,7 @@ class STILContainer(PluginMainWidget):
             payload = response['payload']
             message = payload['message']
             logger.info(message)
+            self.publish_to_log(message, level='INFO')
 
     # --- Public API
     # ------------------------------------------------------------------------
@@ -184,3 +197,29 @@ class STILContainer(PluginMainWidget):
 
     def notify_project_status(self, ate_project_loaded: bool):
         self.run_stil_action.setEnabled(ate_project_loaded)
+
+    def update_font(self, font, color_scheme):
+        """
+        Update font of the code editor.
+
+        Parameters
+        ----------
+        font: QFont
+            Font object.
+        color_scheme: str
+            Name of the color scheme to use.
+        """
+        self.output_log.set_color_scheme(color_scheme)
+        self.output_log.set_font(font)
+
+    def publish_to_log(self, msg: str, skip_time=False, level=None):
+        prefix = ''
+        if level is not None:
+            prefix = f'{level} '
+        if not skip_time:
+            now = datetime.now()
+            prefix = f'{prefix}{now.isoformat()} - '
+        text = self.output_log.toPlainText()
+        line_sep = self.output_log.get_line_separator()
+        text += f'{prefix}{msg}{line_sep}'
+        self.output_log.set_text(text)
