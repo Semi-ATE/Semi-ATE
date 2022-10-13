8f652cda22c5e2e673ec696451b339f996faeae0
Zlin526F <christian.jung@tdk.com>
2022-10-13 11:15:01 +0200

    fix: some basic linting errors

--- src/ATE_spyder/ate_spyder/widgets/actions_on/tests/TestWizard.py
+++ src/ATE_spyder/ate_spyder/widgets/actions_on/tests/TestWizard.py
@@ -28,14 +28,15 @@ from ate_spyder.widgets.validation import valid_fmt_regex
 from ate_spyder.widgets.validation import valid_max_float_regex
 from ate_spyder.widgets.validation import valid_min_float_regex
 from ate_spyder.widgets.validation import valid_name_regex
-from ate_spyder.widgets.validation import valid_name_regex
-from ate_spyder.widgets.actions_on.tests.Utils import POWER
+from ate_spyder.widgets.actions_on.tests.Utils import POWER, Delegator
 from ate_spyder.widgets.constants import UpdateOptions
 from ate_common.parameter import InputColumnKey, InputColumnLabel, OutputColumnKey, OutputColumnLabel
 
 minimal_docstring_length = 80
 
 MAX_OUTPUT_NUMBER = 100
+
+
 @unique
 class InputColumnIndex(Enum):
     SHMOO = 0
@@ -49,6 +50,7 @@ class InputColumnIndex(Enum):
 
     def __call__(self):
         return self.value
+
 
 @unique
 class OutputColumnIndex(Enum):
@@ -66,62 +68,33 @@ class OutputColumnIndex(Enum):
     def __call__(self):
         return self.value
 
-OUTPUT_MAX_NUM_COLUMNS = max([ e.value for e in OutputColumnIndex ]) + 1
-INPUT_MAX_NUM_COLUMNS = max([ e.value for e in InputColumnIndex ]) + 1
+
+OUTPUT_MAX_NUM_COLUMNS = max([e.value for e in OutputColumnIndex]) + 1
+INPUT_MAX_NUM_COLUMNS = max([e.value for e in InputColumnIndex]) + 1
 
 OUTPUT_PARAMETER_COLUMN_MAP = [
-    { 'type': OutputColumnIndex.NAME,  'key': OutputColumnKey.NAME(), 'label': OutputColumnLabel.NAME(),  'default': 'parameterX_name' },
-    { 'type': OutputColumnIndex.LSL,   'key': OutputColumnKey.LSL(),  'label': OutputColumnLabel.LSL(),   'default': -np.inf },
-    { 'type': OutputColumnIndex.LTL,   'key': OutputColumnKey.LTL(),  'label': OutputColumnLabel.LTL(),   'default': np.nan },
-    { 'type': OutputColumnIndex.NOM,   'key': OutputColumnKey.NOM(),  'label': OutputColumnLabel.NOM(),   'default': 0.0 },
-    { 'type': OutputColumnIndex.UTL,   'key': OutputColumnKey.UTL(),  'label': OutputColumnLabel.UTL(),   'default': np.nan },
-    { 'type': OutputColumnIndex.USL,   'key': OutputColumnKey.USL(),  'label': OutputColumnLabel.USL(),   'default': np.inf },
-    { 'type': OutputColumnIndex.POWER, 'key': OutputColumnKey.POWER(),'label': OutputColumnLabel.POWER(), 'default': '˽' },
-    { 'type': OutputColumnIndex.UNIT,  'key': OutputColumnKey.UNIT(), 'label': OutputColumnLabel.UNIT(),  'default': '˽' },
-    { 'type': OutputColumnIndex.FMT,   'key': OutputColumnKey.FMT(),  'label': OutputColumnLabel.FMT(),   'default': '.3f' },
-    { 'type': OutputColumnIndex.MPR,   'key': OutputColumnKey.MPR(),  'label': OutputColumnLabel.MPR(),   'default': False },
+    {'type': OutputColumnIndex.NAME,  'key': OutputColumnKey.NAME(),  'label': OutputColumnLabel.NAME(),  'default': 'parameterX_name'},
+    {'type': OutputColumnIndex.LSL,   'key': OutputColumnKey.LSL(),   'label': OutputColumnLabel.LSL(),   'default': -np.inf},
+    {'type': OutputColumnIndex.LTL,   'key': OutputColumnKey.LTL(),   'label': OutputColumnLabel.LTL(),   'default': np.nan},
+    {'type': OutputColumnIndex.NOM,   'key': OutputColumnKey.NOM(),   'label': OutputColumnLabel.NOM(),   'default': 0.0},
+    {'type': OutputColumnIndex.UTL,   'key': OutputColumnKey.UTL(),   'label': OutputColumnLabel.UTL(),   'default': np.nan},
+    {'type': OutputColumnIndex.USL,   'key': OutputColumnKey.USL(),   'label': OutputColumnLabel.USL(),   'default': np.inf},
+    {'type': OutputColumnIndex.POWER, 'key': OutputColumnKey.POWER(), 'label': OutputColumnLabel.POWER(), 'default': '˽'},
+    {'type': OutputColumnIndex.UNIT,  'key': OutputColumnKey.UNIT(),  'label': OutputColumnLabel.UNIT(),  'default': '˽'},
+    {'type': OutputColumnIndex.FMT,   'key': OutputColumnKey.FMT(),   'label': OutputColumnLabel.FMT(),   'default': '.3f'},
+    {'type': OutputColumnIndex.MPR,   'key': OutputColumnKey.MPR(),   'label': OutputColumnLabel.MPR(),   'default': False},
 ]
 
 INPUT_PARAMETER_COLUMN_MAP = [
-    { 'type': InputColumnIndex.SHMOO,   'key': InputColumnKey.SHMOO(),  'label': InputColumnLabel.SHMOO(),   'default': None },
-    { 'type': InputColumnIndex.NAME,    'key': InputColumnKey.NAME(),   'label': InputColumnLabel.NAME(),    'default': 'parameterX_name' },
-    { 'type': InputColumnIndex.MIN,     'key': InputColumnKey.MIN(),    'label': InputColumnLabel.MIN(),     'default': None },
-    { 'type': InputColumnIndex.DEFAULT, 'key': InputColumnKey.DEFAULT(),'label': InputColumnLabel.DEFAULT(), 'default': None },
-    { 'type': InputColumnIndex.MAX,     'key': InputColumnKey.MAX(),    'label': InputColumnLabel.MAX(),     'default':None },
-    { 'type': InputColumnIndex.POWER,   'key': InputColumnKey.POWER(),  'label': InputColumnLabel.POWER(),   'default': '˽' },
-    { 'type': InputColumnIndex.UNIT,    'key': InputColumnKey.UNIT(),   'label': InputColumnLabel.UNIT(),    'default': '˽' },
-    { 'type': InputColumnIndex.FMT,     'key': InputColumnKey.FMT(),    'label': InputColumnLabel.FMT(),     'default': None },
+    {'type': InputColumnIndex.SHMOO,   'key': InputColumnKey.SHMOO(),   'label': InputColumnLabel.SHMOO(),   'default': None},
+    {'type': InputColumnIndex.NAME,    'key': InputColumnKey.NAME(),    'label': InputColumnLabel.NAME(),    'default': 'parameterX_name'},
+    {'type': InputColumnIndex.MIN,     'key': InputColumnKey.MIN(),     'label': InputColumnLabel.MIN(),     'default': None},
+    {'type': InputColumnIndex.DEFAULT, 'key': InputColumnKey.DEFAULT(), 'label': InputColumnLabel.DEFAULT(), 'default': None},
+    {'type': InputColumnIndex.MAX,     'key': InputColumnKey.MAX(),     'label': InputColumnLabel.MAX(),     'default': None},
+    {'type': InputColumnIndex.POWER,   'key': InputColumnKey.POWER(),   'label': InputColumnLabel.POWER(),   'default': '˽'},
+    {'type': InputColumnIndex.UNIT,    'key': InputColumnKey.UNIT(),    'label': InputColumnLabel.UNIT(),    'default': '˽'},
+    {'type': InputColumnIndex.FMT,     'key': InputColumnKey.FMT(),     'label': InputColumnLabel.FMT(),     'default': None},
 ]
-
-class Delegator(QtWidgets.QStyledItemDelegate):
-    """General Custom Delegator Class that works with regex."""
-
-    def __init__(self, regex, table=None, column=None, parent=None):
-        QtWidgets.QStyledItemDelegate.__init__(self, parent)
-        self.table: QtWidgets.QTableView = table
-        self.column = column
-        self.validator = QtGui.QRegExpValidator(QtCore.QRegExp(regex))
-
-    def createEditor(self, parent, option, index):
-        """Overloading to customize."""
-        line_edit = QtWidgets.QLineEdit(parent)
-        line_edit.setValidator(self.validator)
-        line_edit.editingFinished.connect(lambda: self.validate_text(line_edit))
-
-        return line_edit
-
-    def validate_text(self, line_edit: QtWidgets.QLineEdit):
-        if not keyword.iskeyword(line_edit.text()):
-            return
-
-        if self.table is None or self.column is None:
-            return
-
-        col = self.table.selectionModel().currentIndex().column()
-        row = self.table.selectionModel().currentIndex().row()
-        if col == self.column:
-            item = self.table.model().item(row, col)
-            item.setText("")
 
 
 class NameDelegator(Delegator):
@@ -216,7 +189,7 @@ class TestWizard(BaseDialog):
         self.inputParameterDelete.setIcon(qta.icon('mdi.minus-box-outline', color='orange'))
         self.inputParameterDelete.setToolTip('Delete selected parameter')
 
-        inputParameterHeaderLabels =  list(
+        inputParameterHeaderLabels = list(
             map(lambda parameter: parameter['label'],
                 list(sorted(INPUT_PARAMETER_COLUMN_MAP, key=lambda parameter: parameter['type']())))
         )
@@ -263,7 +236,7 @@ class TestWizard(BaseDialog):
         self.outputParameterDelete.setIcon(qta.icon('mdi.minus-box-outline', color='orange'))
         self.outputParameterDelete.setToolTip('Delete selected parameter')
 
-        outputParameterHeaderLabels =  list(
+        outputParameterHeaderLabels = list(
             map(lambda parameter: parameter['label'],
                 list(sorted(OUTPUT_PARAMETER_COLUMN_MAP, key=lambda parameter: parameter['type']())))
         )
@@ -1247,7 +1220,7 @@ class TestWizard(BaseDialog):
         selection = self.outputParameterView.selectedIndexes()
 
         for index in selection:
-            if index.column() == OutputColumnIndex.UNIT(): 
+            if index.column() == OutputColumnIndex.UNIT():
                 self.outputParameterModel.setData(index, text, QtCore.Qt.DisplayRole)
                 self.outputParameterModel.setData(index, tooltip, QtCore.Qt.ToolTipRole)
         self.outputParameterView.clearSelection()
@@ -1309,7 +1282,7 @@ class TestWizard(BaseDialog):
             MPR_item.setData(QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
         else:
             MPR_item.setData(QtCore.Qt.Checked, QtCore.Qt.CheckStateRole)
-        
+
     # LSL
         if attributes[OutputColumnKey.LSL()] is None:  # None on LSL doesn't exist --> = -Inf
             LSL_ = -np.inf
@@ -1472,7 +1445,7 @@ class TestWizard(BaseDialog):
         the result is always a float (might be np.inf or np.nan) for LSL, LTL, Nom, UTL and USL (rest is string)
         the source is in the model, and always a string.
         '''
-        attributes = make_default_output_parameter(empty = True)
+        attributes = make_default_output_parameter(empty=True)
         name_item = self.outputParameterModel.item(row, OutputColumnIndex.NAME())
         name = name_item.data(QtCore.Qt.DisplayRole)
 
@@ -1837,7 +1810,6 @@ class TestWizard(BaseDialog):
 
         return target[start_index: end_index]
 
-
     @QtCore.pyqtSlot(int)
     def _group_selected(self, index: int):
         item = self.group_combo.model().item(index, 0)
@@ -1859,18 +1831,19 @@ def make_blank_definition(project_info):
     retval['hardware'] = project_info.active_hardware
     retval['base'] = project_info.active_base
     retval['docstring'] = []
-    retval['input_parameters'] = {'Temperature': make_default_input_parameter(temperature=True) }
-    retval['output_parameters'] = { 'new_parameter1': make_default_output_parameter() }
+    retval['input_parameters'] = {'Temperature': make_default_input_parameter(temperature=True)}
+    retval['output_parameters'] = {'new_parameter1': make_default_output_parameter()}
     retval['dependencies'] = {}
     retval['patterns'] = []
     return retval
+
 
 def make_default_input_parameter(temperature: bool = False):
     input_parameters_without_name = list(filter(lambda e: e['type'] != InputColumnIndex.NAME, INPUT_PARAMETER_COLUMN_MAP))
     input_key_list = list(map(lambda e: e['key'], input_parameters_without_name))
     input_default_list = list(map(lambda e: e['default'], input_parameters_without_name))
     default_input_parameter = dict(zip(input_key_list, input_default_list))
-    if temperature == True:
+    if temperature is True:
         default_input_parameter[InputColumnKey.SHMOO()] = True
         default_input_parameter[InputColumnKey.MIN()] = -40
         default_input_parameter[InputColumnKey.DEFAULT()] = 25
@@ -1879,12 +1852,13 @@ def make_default_input_parameter(temperature: bool = False):
         default_input_parameter[InputColumnKey.FMT()] = '.0f'
     return default_input_parameter
 
+
 def make_default_output_parameter(empty: bool = False):
     output_parameters_without_name = list(filter(lambda e: e['type'] != OutputColumnIndex.NAME, OUTPUT_PARAMETER_COLUMN_MAP))
     output_key_list = list(map(lambda e: e['key'], output_parameters_without_name))
     output_default_list = list(map(lambda e: e['default'], output_parameters_without_name))
     default_output_parameter = dict(zip(output_key_list, output_default_list))
-    if empty == True:
+    if empty is True:
         default_output_parameter[OutputColumnKey.LSL()] = None
         default_output_parameter[OutputColumnKey.LTL()] = None
         default_output_parameter[OutputColumnKey.NOM()] = None
