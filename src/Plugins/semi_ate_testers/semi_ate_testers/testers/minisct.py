b5a2620311b4e9ac53df3203011a688bd8143ab9
nerohmot <thor@semi-ate.com>
2022-11-18 14:35:52 +0000

    replace print with self.log_, add teardown

--- src/Plugins/semi_ate_testers/semi_ate_testers/testers/minisct.py
+++ src/Plugins/semi_ate_testers/semi_ate_testers/testers/minisct.py
@@ -1,10 +1,7 @@
 import platform
 from semi_ate_testers.testers.tester_interface import TesterInterface
+from SCT8.tester import Tester as sct8
 
-if "linux" in platform.system().lower() and "aarch64" in platform.machine().lower():
-    from SCT8.tester import Tester as sct8
-else:
-    from SCT8.dummy_tester import Tester as sct8
 
 
 class MiniSCT(TesterInterface, sct8):
@@ -12,21 +9,26 @@ class MiniSCT(TesterInterface, sct8):
 
     def __init__(self, logger=None):
         TesterInterface.__init__(self, logger)
-        sct8.__init__(self, logger)
+        sct8.__init__(self)
 
     def do_request(self, site_id: int, timeout: int) -> bool:
+        self.log_info(f'MiniSCT.do_request(site_id={site_id})')
         return True
 
     def test_in_progress(self, site_id: int):
-        print(f'Tester.test_in_progress({site_id})')
+        self.log_info(f'MiniSCT.test_in_progress(site_id={site_id})')
 
     def test_done(self, site_id: int, timeout: int):
-        print(f'Tester.test_done({site_id})')
+        self.log_info(f'MiniSCT.test_done({site_id})')
 
     def do_init_state(self, site_id: int):
-        print(f'Tester.do_init_state({site_id})')
+        self.log_info(f'MiniSCT.do_init_state(site_id={site_id})')
         if hasattr(sct8, 'do_init_state'):
-            sct8.init_hardware(self)
+            sct8.do_init_state(self)
+
+    def teardown(self):
+        self.log_info('MiniSCT.teardown')
+        sct8.close(self)
 
     def run_pattern(self, pattern_name: str, start_label: str = '', stop_label: str = '', timeout: int = 1000):
         self.pf.run(pattern_name, start_label, stop_label, timeout)
