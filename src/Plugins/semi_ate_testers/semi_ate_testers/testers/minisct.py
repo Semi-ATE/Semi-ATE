0e7c4346034618a47583d002df91b23f3b3f4079
nerohmot <thor@semi-ate.com>
2023-02-24 08:53:07 +0000

    adjustment for tester lib

--- src/Plugins/semi_ate_testers/semi_ate_testers/testers/minisct.py
+++ src/Plugins/semi_ate_testers/semi_ate_testers/testers/minisct.py
@@ -1,15 +1,12 @@
-import platform
 from semi_ate_testers.testers.tester_interface import TesterInterface
-from SCT8.tester import Tester as sct8
+from SCT8.tester import TesterImpl
 
 
-
-class MiniSCT(TesterInterface, sct8):
+class MiniSCT(TesterInterface, TesterImpl):
     SITE_COUNT = 1
 
     def __init__(self, logger=None):
         TesterInterface.__init__(self, logger)
-        sct8.__init__(self)
 
     def do_request(self, site_id: int, timeout: int) -> bool:
         self.log_info(f'MiniSCT.do_request(site_id={site_id})')
@@ -22,13 +19,13 @@ class MiniSCT(TesterInterface, sct8):
         self.log_info(f'MiniSCT.test_done({site_id})')
 
     def do_init_state(self, site_id: int):
+        TesterImpl.__init__(self)
         self.log_info(f'MiniSCT.do_init_state(site_id={site_id})')
-        if hasattr(sct8, 'do_init_state'):
-            sct8.do_init_state(self)
+        self.turnOn()
 
     def teardown(self):
         self.log_info('MiniSCT.teardown')
-        sct8.close(self)
+        self.turnOff()
 
     def run_pattern(self, pattern_name: str, start_label: str = '', stop_label: str = '', timeout: int = 1000):
         self.pf.run(pattern_name, start_label, stop_label, timeout)
