d19efed4e309b266e09ce504a47c8216ad1ec482
Zlin526F <christian.jung@tdk.com>
2023-03-17 09:51:15 +0100

    move generating list from protocols to widgets/navigation.py

--- src/ATE_common/ate_common/pattern/stil_tool_base.py
+++ src/ATE_common/ate_common/pattern/stil_tool_base.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 from pathlib import Path
 from abc import ABC, abstractmethod
 
@@ -8,42 +6,16 @@ class StilToolBase(ABC):
     def __init__(self):
         self._compiled_patterns = None
 
-    def getPath(self):
-        '''get the necesary information about hardware, base, target
-        #TODO!   this should  do the STIL-Widget
-        '''
-        import __main__
-
-        mainFileSplit = __main__.__file__.split(os.sep)
-        cwd = str(Path(__main__.__file__).parent.parent.parent.parent)
-        filename = os.path.splitext(mainFileSplit[-1])[0]
-        target = mainFileSplit[-1].split('_')[-3]
-        base = mainFileSplit[-2]
-        hardware = mainFileSplit[-3]
-        projectname = mainFileSplit[-4]
-        return cwd, os.path.join(hardware, base, target), filename
-
-    def _add_protocols(self, compiled_pattern_list):
-        cwd, relPath, projectFilename = self.getPath()
-        outputdir = cwd + os.sep + 'pattern_output'
-        for root, directories, files in os.walk(os.path.join(cwd, "protocols", relPath)):
-            for filename in files:
-                if filename.endswith("stil.hdf5"):
-                    compiled_pattern_list.append(os.path.join(outputdir, filename))
-                    shutil.copy(os.path.join(root, filename), os.path.join(outputdir, filename))
-        return compiled_pattern_list
-
     def _load_patterns(self, compiled_patterns: dict):
         self._compiled_patterns = compiled_patterns
 
-        compiled_pattern_list = self._add_protocols(list(set(compiled_patterns.values())))
-
-        if not compiled_pattern_list:
-            print('-------- there is no pattern/protocols to load ---------')
+        if not self._compiled_patterns:
+            print('-------- there is no pattern to load ---------')
             return
 
         # patterns could be assigned for multiple tests
         # retrieve all the patterns so they appear only once in the list
+        compiled_pattern_list = list(set(compiled_patterns.values()))
         # make sure compiled patterns exists
         for compiled_pattern in compiled_pattern_list:
             if not Path(compiled_pattern).exists():
