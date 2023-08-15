83d7744391fdea90e158ac13cacded833f8b2682
Zlin526F <christian.jung@tdk.com>
2023-08-15 12:12:57 +0200

    fix issue 330, when stil compiler not available

--- src/ATE_common/ate_common/pattern/dummy_stil_tool.py
+++ src/ATE_common/ate_common/pattern/dummy_stil_tool.py
@@ -2,5 +2,5 @@ from ate_common.pattern.stil_tool_base import StilToolBase
 
 
 class DummyStilTool(StilToolBase):
-    def _load_patterns_impl(self, compiled_patterns: dict):
+    def _load_patterns_impl(self, compiled_patterns: dict, sig2chan=None):
         print('the environment doesn\'t support loading patterns')
