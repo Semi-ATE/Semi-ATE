403c56cdd81cb3e9a1ec7fbc74b74a6e74b47cd8
Zlin526F <christian.jung@tdk.com>
2023-08-29 15:51:30 +0000

    restore invalid issue 330

--- src/ATE_sammy/ate_sammy/coding/TestRunnerMainGenerator.py
+++ src/ATE_sammy/ate_sammy/coding/TestRunnerMainGenerator.py
@@ -32,7 +32,7 @@ class test_runner_generator(BaseGenerator):
         if file_path.exists():
             os.remove(file_path)
 
-        compiled_patterns = collect_compiled_patterns(test_configuration.definition['patterns'], self.project_path, None)
+        compiled_patterns = collect_compiled_patterns(test_configuration.definition['patterns'], self.project_path)
 
         output = template.render(
             project_name=project_path.name,
