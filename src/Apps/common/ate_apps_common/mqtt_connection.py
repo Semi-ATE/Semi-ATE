52687efbd37a28b74396c5b766d6c7f49f0bc2ec
Zlin526F <christian.jung@tdk.com>
2024-08-08 15:48:39 +0200

    fix try with an old version from aiomqtt: Client.__init__() got an unexpected keyword argument

--- src/Apps/common/ate_apps_common/mqtt_connection.py
+++ src/Apps/common/ate_apps_common/mqtt_connection.py
@@ -8,12 +8,12 @@ from ate_common.logger import LogLevel, Logger
 
 class MqttConnection:
     def __init__(self, host: str, port: int, mqtt_client_id: str, logger: Logger):
-        print('MqttConnection: ', host, port, mqtt_client_id )
         if aiomqtt.__version__ <= '0.1.3':
             self.mqtt_client = aiomqtt.Client(client_id=mqtt_client_id)
+            self.mqtt_client.reconnect_delay_set(10, 15)
         else:
             self.mqtt_client = aiomqtt.Client(mqtt_client_id)
-        self.mqtt_client.reconnect_delay_set(10, 15)
+            #Todo:  actual Version from aiomqtt is > 2.3   -> some things need to be changed
         self.log = logger
         self.host = host
         self.port = port
