cde94a90cee2a6c59a3f1981e59733354386312b
Christian Jung <christian.jung@tdk.com>
2025-08-18 10:22:42 +0200

    fix issue 357

--- src/ATE_common/ate_common/pattern/dummy_stil_tool.py
+++ src/ATE_common/ate_common/pattern/dummy_stil_tool.py
@@ -3,4 +3,5 @@ from ate_common.pattern.stil_tool_base import StilToolBase
 
 class DummyStilTool(StilToolBase):
     def _load_patterns_impl(self, compiled_patterns: dict, sig2chan=None):
-        print('the environment doesn\'t support loading patterns')
+        print('Stil Tool not available, the environment doesn\'t support loading patterns')
+        print('   There is an example on https://github.com/Semi-ATE/STIL showing how you can integrate a style parser.')
