1cb802c20e4eb82b8f41acdbda1145bbb941f495
Christian Jung <christian.jung@tdk.com>
2026-07-13 14:12:45 +0200

    fix errors with pytest-asyncio 0.18.2 → 0.2.8

--- src/integration_tests/conftest.py
+++ src/integration_tests/conftest.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 import sys
 import asyncio
+import pytest
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers",
+        "asyncio: mark test as asyncio"
+    )
+
+# Windows Event Loop Policy
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())    
